240abf6afd1e34cf78aa95a35c9181e3f005d34b
ChrisAruiza1015 <chrisaruiza57@gmail.com>
2022-01-07 12:53:31 -0800

    First commit of my React app!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyApp from './MyApp'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +17,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
